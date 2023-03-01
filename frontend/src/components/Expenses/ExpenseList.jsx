80676f7867c26e33ede99b7192d20466ef49c4eb
Sean <seanboateng2@gmail.com>
2023-02-28 19:55:31 -0500

    User can now add expenses to project

--- frontend/src/components/Expenses/ExpenseList.jsx
+++ frontend/src/components/Expenses/ExpenseList.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
 
 
 
 const ExpenseList = (props) => {
 
-
+    
 
 
     return ( 
         <div>
             {props.expense.map((el)=>{
                 return(
-                    <div>
-                        {el.title}
-                        {el.amount}
-                        {el.category}
-                    </div>
+                    <tr>
+                        <td>{el.title}</td>
+                        <td>{el.amount}</td>
+                        <td>{el.category}</td>                
+                    </tr>
                 )
             })}
 
--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -5,15 +5,18 @@ import useAuth from "../../hooks/useAuth";
 import { useNavigate } from 'react-router-dom';
 import Sidebar from '../SideBar/sidebar';
 import ExpenseList from '../Expenses/ExpenseList';
+import AddProject from '../Add Project/AddProject';
 
 
 const LandingPage = (props) => {
     const [project,setProject] = useState([])
-    const [projectid,setProjectid] = useState('')
-    const [expense,setExpense] = useState('')
+    // const [projectid,setProjectid] = useState('')
+    const [expense,setExpense] = useState([])
     const [saveid,setSaveid] = useState([])
+    const [displayform,setDisplayForm] = useState([])
     const [user, token] = useAuth();
     const navigate = useNavigate()
+    
 
    
 
@@ -22,7 +25,7 @@ const LandingPage = (props) => {
     useEffect(() => {
         getAllProjects();
         getProjectExpenses();
-        AddProject();
+        
       }, [token])
 
       async function getAllProjects (props){
@@ -41,30 +44,31 @@ const LandingPage = (props) => {
 
       async function getProjectExpenses(projectid){
         try {
-          let response = await axios.get(`http://127.0.0.1:8000/api/project/${projectid}/expense/`, {
+          let response = await axios.get(`http://127.0.0.1:8000/api/project/${projectid}/expense/all`, {
             headers: {
               Authorization: "Bearer " + token,
             },
           });
           setExpense(response.data);
+          setSaveid(projectid)
         } catch (error) {
           console.log(error.response.data);
         }
       };
 
-      async function AddProject(newEntry){
+      async function AddExpense(newEntry){
         try {
-          let response = await axios.post(`http://127.0.0.1:8000/api/project/${saveid}/expense/`, newEntry, {
+          let response = await axios.post(`http://127.0.0.1:8000/api/project/${saveid}/expense/all`,newEntry, {
             headers: {
               Authorization: "Bearer " + token,
+
             },
-          });
-          if (response.status===201){
-            await getAllProjects()
-          }
+          } );
+          
         } catch (error) {
           console.log(error.response.data);
         }
+        
       };
       
      
@@ -74,12 +78,20 @@ const LandingPage = (props) => {
 
     return ( 
       <div>
-        <Sidebar project = {project} projectid = {getProjectExpenses}/>
+        <Sidebar project = {project} projectid = {getProjectExpenses} />
         <ExpenseList expense = {expense}/>
-        <AddProject addProject = {AddProject}/>
+        
+        
+
+
+        {displayform ? <button onClick={()=>setDisplayForm(!displayform)}>Add </button>: <AddProject add = {AddExpense}/>}<br/>
+              {/* <button >Add New Card</button> */}
+
+
       </div>
      );
 }
+// addExpense = {AddExpense}
  
 export default LandingPage;
 
